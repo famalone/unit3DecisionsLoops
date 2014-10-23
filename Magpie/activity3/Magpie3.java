ca4b3f8e2a482d569c99e38cfdd3c1a5ce20ae5b
famalone <finmalone@gmail.com>
2014-10-23 11:19:22 -0500

    even More Magpie!!

--- Magpie/activity3/Magpie3.java
+++ Magpie/activity3/Magpie3.java
@@ -166,7 +166,7 @@ public class Magpie3
      */
     private String getRandomResponse()
     {
-        final int NUMBER_OF_RESPONSES = 4;
+        final int NUMBER_OF_RESPONSES = 6;
         double r = Math.random();
         int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
         String response = "";
@@ -186,6 +186,14 @@ public class Magpie3
         else if (whichResponse == 3)
         {
             response = "You don't say.";
+        }
+        else if (whichResponse == 4)
+        {
+            response = "How dare you";
+        }
+        else if (whichResponse == 5)
+        {
+            response = "I agree.";
         }
 
         return response;
